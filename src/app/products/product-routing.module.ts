15a73dd876f557f981c816578c72e2ead6c597eb
sink-opuba <osinkopere@yahoo.com>
2019-10-07 21:01:52 +0100

    refactor app module into separate modules

---
+++ src/app/products/product-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailGuard } from './product-detail.guard';
+import { ProductDetailComponent } from './product-detail.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild([
+    { path: "products", component: ProductListComponent },
+    { path: "products/:id", canActivate: [ProductDetailGuard], component: ProductDetailComponent }
+  ])
+  ],
+  exports: [RouterModule]
+})
+export class ProductRoutingModule { }
